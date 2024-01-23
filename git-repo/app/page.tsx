2143733c21a934827b3a0f877a8673bf753074ca
abiraj159 <abiraj159@gmail.com>
2024-01-24 00:59:49 +0530

    Some more field commented out

--- git-repo/app/page.tsx
+++ git-repo/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <h2>Git Repo</h2>
       </div> */}
 
-      <div className={styles.grid}>
+      {/* <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
@@ -74,7 +74,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
